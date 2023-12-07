2b203470a21826bfeb6fc15cbe7d6bf15926e6a5
Vladimir.Zhitkevich@outlook.com <Vladimir.Zhitkevich@outlook.com>
2023-12-08 01:22:07 +0300

    Add mudBlazor template

---
+++ DanceJournal.MudWeb/Journal/Pages/LessonsPlanningComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using DanceJournal.MudWeb.Journal.Models;
+
+namespace DanceJournal.MudWeb.Journal.Pages;
+
+partial class LessonsPlanningComponent
+{
+    private IEnumerable<ELement>? Elements;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Elements = new List<ELement>()
+        {
+            new ELement()
+            {
+                Number = 1,
+                Molar = "1587.7",
+                Name = "Name",
+                Position = "115",
+                Sign = "H"
+            }
+        };
+    }
+}
