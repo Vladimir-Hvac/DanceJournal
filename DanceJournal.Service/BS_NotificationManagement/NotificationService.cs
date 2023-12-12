6ceab690ad469b8de839dd55de14289988c0831f
AleeexWind <alex_wind@mail.ru>
2023-12-12 19:09:51 +0300

    Initial NotificationService

---
+++ DanceJournal.Service/BS_NotificationManagement/NotificationService.cs
@@ -0,0 +1,27 @@
+﻿using DanceJournal.Service.BS_NotificationManagement.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanceJournal.Service.BS_NotificationManagement
+{
+    public interface INotificationService
+    {
+        List<NotificationDTO> GetNotReadNotifications(UserAuthData userAuthData);
+        NotificationDTO ReadNotification(int notificationId);
+        bool MarkAsRead(int notificationId);
+        bool AcceptInvitation(int notificationId);
+        bool DeclineInvitation(int notificationId);
+
+
+        List<EventDTO> ProvideEvents(int userId);
+        List<UserDTO> ProvideRecipients(int eventId);
+        bool SentInvitation(int eventId, List<int> recipientsIds);
+    }
+
+    public class NotificationService
+    {
+    }
+}
