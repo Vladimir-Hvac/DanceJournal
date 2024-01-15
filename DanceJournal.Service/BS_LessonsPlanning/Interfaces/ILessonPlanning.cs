3238eb37cc942463341203b84c02ef23e7237d81
fedorzavaley <fedorzavaley@gmail.com>
2024-01-15 22:31:55 +0300

    Заменен интерфейс репозитория и методы BS_LessonsPlanning

--- DanceJournal.Service/BS_LessonsPlanning/Interfaces/ILessonPlanning.cs
+++ DanceJournal.Service/BS_LessonsPlanning/Interfaces/ILessonPlanning.cs
@@ -1,9 +1,21 @@
+using DanceJournal.Infrastructure.Repository;
+
 public interface ILessonPlanning
 {
-    public Task<IEnumerable<LP_Lesson>> GetAllLessons();
-    public Task<LP_Lesson> GetLesson(int Id);
-    public Task AddLesson(LP_Lesson lesson);
-    public Task UpdateLesson(LP_Lesson lesson);
-    public Task RemoveLesson(int id);
-    public Task<IEnumerable<LP_Room>> GetAllRooms();
+    //Получить список всех занятий
+    public IEnumerable<Lesson> GetAllLessons();
+    //Получить информацию о занятии
+    public Lesson GetLesson(Lesson lesson);
+    //Добавить занятие
+    public Task AddLesson(Lesson lesson);
+    //Редактировать занятие
+    public Task UpdateLesson(Lesson lesson);
+    //Удалить занятие
+    public Task RemoveLesson(Lesson lesson);
+    //Получить список всех залов
+    public IEnumerable<Room> GetAllRooms();
+    //Забронировать зал
+    //При отправке Room бронируется зал для урока
+    //При отправке Room = null, бронирование снимается, а в Lesson.RoomId заполняется 0
+    public Task BookRoom(Lesson lesson, Room room);
 }
