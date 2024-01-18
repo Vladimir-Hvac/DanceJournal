50845b48519c2287033cd9c2b9f89a019abea314
Vladimir.Zhitkevich@outlook.com <Vladimir.Zhitkevich@outlook.com>
2024-01-18 21:31:55 +0300

    Add calendar, Lessons

--- DanceJournal.MudWeb/Journal/Pages/LessonsPlanningComponent.razor.cs
+++ DanceJournal.MudWeb/Journal/Pages/LessonsPlanningComponent.razor.cs
@@ -2,42 +2,45 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 
-namespace DanceJournal.MudWeb.Journal.Pages;
-
+namespace DanceJournal.MudWeb.Journal.Pages
 {
-    [Inject]
-    public DanceJournalDbContext DjDbContext { get; set; }
-
-    protected override async Task OnInitializedAsync()
+    partial class LessonsPlanningComponent
     {
-        //DjDbContext.Users.Add(new User() { Name = "Peter" });
-        //DjDbContext.SaveChanges();
+        private List<Lesson>? Lessons;
+        private string _searchString;
+        private bool _isCellEditMode;
+        private bool _readOnly;
+
+        protected override async Task OnInitializedAsync()
         {
+            Lessons = new List<Lesson>();
+            Lessons.Add(
+                new Lesson()
+                {
+                    Date = DateTime.Now,
+                    LessonType = new LessonType() { Name = "Танго", Price = 1200 },
+                    Level = new Level() { Coefficient = 0.5, Title = "F" },
+                    Room = new Room() { Name = "Танц зал 1" },
+                }
+            );
+        }
+
+        private void StartedEditingItem(Lesson lessonType) { }
+
+        private void CanceledEditingItem(Lesson lessonType) { }
+
+        private void CommittedItemChanges(Lesson lessonType) { }
+
+        private Func<Lesson, bool> _quickFilter =>
+            x =>
             {
-            }
+                if (string.IsNullOrWhiteSpace(_searchString))
+                    return true;
 
-    private void StartedEditingItem(LessonTypeDto lessonType) { }
+                if (x.LessonType.Name.Contains(_searchString, StringComparison.OrdinalIgnoreCase))
+                    return true;
 
-    private void CanceledEditingItem(LessonTypeDto lessonType) { }
-
-    private void CommittedItemChanges(LessonTypeDto lessonType) { }
-
-    private Func<LessonTypeDto, bool> _quickFilter =>
-        x =>
-        {
-            if (string.IsNullOrWhiteSpace(_searchString))
-                return true;
-
-            if (x.Name.Contains(_searchString, StringComparison.OrdinalIgnoreCase))
-                return true;
-
-            if (x.Type.Contains(_searchString, StringComparison.OrdinalIgnoreCase))
-                return true;
-
-            if ($"{x.Price}".Contains(_searchString))
-                return true;
-
-            return false;
-        };
+                return false;
+            };
     }
 }
