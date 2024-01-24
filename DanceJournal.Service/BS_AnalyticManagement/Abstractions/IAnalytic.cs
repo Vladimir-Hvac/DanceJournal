208b09dda93e9a033b75a371bcb6e97d1b307e98
valera <azarbox@mail.ru>
2024-01-24 19:21:09 +0300

    feat: начал делать логи в Nosql

---
+++ DanceJournal.Service/BS_AnalyticManagement/Abstractions/IAnalytic.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Nodes;
+
+
+namespace DanceJournal.Services.BS_AnalyticManagement.Abstractions
+{
+    public interface IAnalytic
+    {
+        Task<bool> SaveLog(dynamic json, string typeEvent);
+    }
+}
